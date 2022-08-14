d83de36d92dbec476fd9b0314d1b516f695fceae
AlexWu20061107 <s15357@qc.edu.hk>
2022-08-14 19:47:15 +0800

    minor fix

--- smw_web/urls.py
+++ smw_web/urls.py
@@ -1,4 +1,4 @@
-"""socs_web URL Configuration
+"""smw_web URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
